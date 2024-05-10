1590acede582b5e8a01bb927026d09478783dba2
stalker1574 <167695363+stalker1574@users.noreply.github.com>
2024-05-10 19:10:58 +0300

    Add files via upload

--- pin_pong.py
+++ pin_pong.py
@@ -18,33 +18,57 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 150:
             self.rect.y += self.speed
 
 window = display.set_mode((600, 500))
 window.fill((200, 250, 255))
 win_height = 500
+win_width = 600
 player1 = Player('bullet.png', 30, 200, 4, 50, 150)
 player2 = Player('bullet.png', 520, 200, 4, 50, 150)
+ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
+speed_x = 3
+speed_y = 3
+finish = False
+
+
 
 game = True
 clock = time.Clock()
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-
-    window.fill((200, 250, 255))
-    player1.update_l()
-    player2.update_r()
-    player1.reset()
-    player2.reset()
+    if not finish:
+        window.fill((200, 250, 255))
+        player1.update_l()
+        player2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+        player1.reset()
+        player2.reset()
+        ball.reset()
     display.update()
     clock.tick(60)
